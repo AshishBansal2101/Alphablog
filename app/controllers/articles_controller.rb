0a8ed15b888961b17b8e7ae35fc0c29481e6a980
Ashish <bansalashish2101@gmail.com>
2023-03-08 13:28:19 +0530

    added articles functionalitues

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,60 @@
 class ArticlesController < ApplicationController
+    before_action :set_article,only: [:edit, :update, :show, :destory]
 
     def show
-        @article=Article.find(params[:id])
+        
     end
 
     def index
          @articles=Article.all
+    end
+
+    def new
+        @article=Article.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+        # render plain: params[:article]
+        @article=Article.new(article_params)
+        #  render plain: @article.inspect
+        
+        if @article.save   
+            flash[:notice]="Article Was Created Successfully"
+            redirect_to @article  
+        else
+            render 'new'
+        end
+
+    end
+
+    def update
+        
+        if @article.update(article_params)
+            flash[:notice]="Article updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+      
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article 
+        @article=Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles ,only: [:show,:index]
+  root 'pages#home'
+  get 'about' , to: 'pages#about'
+  resources :articles
 end
