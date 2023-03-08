c97af58a39555f340a3e2f8e0989a8c8b546f8de
Ashish <bansalashish2101@gmail.com>
2023-03-08 22:55:05 +0530

    add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
         @user=User.new
     end
 
+    def create
+        @user=User.new(user_params)
+        
+        if @user.save   
+            flash[:notice]="welcome to alpha blog #{@user.username} , Your account Was Created Successfully"
+            redirect_to articles_path 
+        else
+            render 'new'
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
+    end
+
 end
